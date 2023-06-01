53ea21db43e864d0263ab53202a4dd2c193a9971
77infinite <123508309+77infinite@users.noreply.github.com>
2023-06-01 13:48:02 -0300

    Update equação de 2°.py

--- equação de 2°.py
+++ equação de 2°.py
@@ -3,16 +3,23 @@ import math
 A = float(input('Digite o valor do A da expressão: '))
 B = float(input('Digite o valor do B da expressão: '))
 C = float(input('Digite o valor do C da expressão: '))
-Delta = B**B -4.0 * A * C
+Delta = (B^2) + (-4.0 * A * C)
 print("Valor de Delta: ", Delta)
 
 if Delta < 0:
     print("Não possui raizes")
-else:  
+elif(Delta > 0):
     Raiz = math.sqrt(Delta)
     X1 = (-B + Raiz)/(2*A)
     X2 = (-B - Raiz)/(2*A)
     print('As raizes desta equação são: ', X1, X2)
+    
+elif (Delta == 0):
+    print("Somente uma raiz")
+    Raiz = math.sqrt(Delta)
+    X1 = (-B + Raiz)/(2*A)
+    X2 = (-B - Raiz)/(2*A)
+    print('A raiz desta equação é: ', X1)
 
 Xv = -B/ (2*A)
 Yv = -Delta/ (4/A)
